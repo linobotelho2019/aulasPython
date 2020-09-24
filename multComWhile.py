cc8509fab7587421ea928e91d9d980c4f0ac5c53
lino botelho <linobotelho2015@hotmail.com>
2020-09-24 01:03:33 -0300

    decrescente

--- multComWhile.py
+++ multComWhile.py
@@ -1,8 +1,7 @@
-tamanho = int(input("Digite o tamanho da sequência de números: "))
-produto = 1
-i = 0
-while i < tamanho:
-    valor = int(input("Digite um valor a ser multiplicado: "))
-    produto = produto * valor
-    i = i + 1
-print("O produto dos valores digitados é: ", produto)
+count = 0
+while (count < 10):
+     # Ponto A
+     print ("Olá...", count)
+     count =  count + 1
+     # Ponto B
+# Ponto C
